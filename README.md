dedbcacd8899dbb488fc58f0e6065325e845dc13
Rohit Pache <rohitpache299@>
2022-10-15 14:54:28 +0530

    updated

--- README.md
+++ README.md
@@ -89,3 +89,7 @@ To stop docker container
 '''
 docker stop container_id
 '''
+
+'''
+python setup.py install
+'''
---
+++ setup.py
@@ -0,0 +1,39 @@
+from importlib.resources import Package
+from pytz import VERSION
+from setuptools import setup
+from typing import List
+
+# declaring variables for setup functions
+
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.1"
+AUTHOR="Rohit Pache"
+DESCRIPTION="This is my first Machine Learning Project that i learned throgh the ineuron"
+PACKAGES=["housing"]
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+def get_requirements_list()->List[str]:
+    
+    """
+    Description: This function is going to return list of requirement 
+    mention in requirement.txt file 
+
+    this function is going to return list which contain name of libraries mention in requirements.txt file
+
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+
+
+setup(
+
+name=PROJECT_NAME,
+version=VERSION,
+author=AUTHOR,
+description=DESCRIPTION,
+packages=PACKAGES,
+install_requires=get_requirements_list()
+
+)
+
